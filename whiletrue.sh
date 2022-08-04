6fce2b2a4992a3c47422ce7794b921ab5071760b
zsjshao <38467891+zsjshao@users.noreply.github.com>
2022-08-04 22:52:54 +0800

    Update whiletrue.sh

--- whiletrue.sh
+++ whiletrue.sh
@@ -2,6 +2,6 @@
 while true
 do
   cd /data/00_hugo && git pull &>/dev/null
-  killall -0 hugo || hugo server -b doc.zsjshao.cn --bind 0.0.0.0
+  killall -0 hugo || hugo server -b 114.132.121.138 --bind 0.0.0.0
   sleep 60
 done
