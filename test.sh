d98ca9f2153ff56cf77c1b569986615477ee5a62
zsjshao <38467891+zsjshao@users.noreply.github.com>
2022-08-04 22:50:16 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo $0
+#read -p "please your name: " name
+#echo $name
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+echo $#
+test1="$@"
+test2="$*"
+echo $test1
+echo $test2
